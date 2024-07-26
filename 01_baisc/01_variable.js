55e81cd7cad3f613759c331fc01cb31f0b1a568c
tejavi8686 <tejasviraj8686@gmail.com>
2024-07-26 13:47:12 +0530

    01_basic

---
+++ 01_baisc/01_variable.js
@@ -0,0 +1,15 @@
+// const value is not changes when it's declared
+const accountId = 14453;
+
+//
+let accountemail = "tejasviraj8686#gmail.com";
+
+// var is not used in that time  prefer not to use war bevause of issue block scope and fucntion scope
+var accountpassword = "12345";
+accountCity = "jaipur";
+let accountstate;
+
+
+//not allowed // accountId = 2;
+console.log(accountId);
+console.log(accountstate);
