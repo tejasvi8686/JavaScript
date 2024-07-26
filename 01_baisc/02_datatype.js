086b26b0e7b0ec63fd1ddc2fb36bb143418976de
tejavi8686 <tejasviraj8686@gmail.com>
2024-07-26 14:48:09 +0530

    converstion of data type

---
+++ 01_baisc/02_datatype.js
@@ -0,0 +1,20 @@
+"use Strict"; // treat all js code as new version
+
+// alert(3+3) we are using node.js not browser
+
+
+let name = "tejasvi"
+let age = "18"
+let isLoggedIn = false
+
+// number => 2 to power 53
+// biginit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// objects 
+
+console.log(typeof "tejavi");
