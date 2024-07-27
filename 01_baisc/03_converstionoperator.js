7fd0840f698b6a7f1acbb6dd321cc6720b45b650
tejavi8686 <tejasviraj8686@gmail.com>
2024-07-27 09:25:57 +0530

    conversion operator and comparison'

--- 01_baisc/03_converstionoperator.js
+++ 01_baisc/03_converstionoperator.js
@@ -4,7 +4,7 @@ let score = "33";
 // console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 
 // "22" => 33
@@ -14,8 +14,7 @@ console.log(typeof valueInNumber);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
@@ -25,4 +24,17 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************************** operation *********************************************
+
+let value =  3
+
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " tejasvi"
+
+let str3 = str1 + str2;
+console.log(str3);
