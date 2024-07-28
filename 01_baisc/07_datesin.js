a5b945655150639c190ae2f9a12671956c0b155a
tejavi8686 <tejasviraj8686@gmail.com>
2024-07-28 08:04:55 +0530

    datein.js

---
+++ 01_baisc/07_datesin.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+console.log(myDate.toString());
+// when the toDateString call it's show day month date time 
+console.log(myDate.toDateString());
+//  when the toLocalString() call  it's shows dat/month/year , time
+console.log(myDate.toLocaleString());
+// only retutn date.month/year when the toLOcalDateString()
+console.log(myDate.toLocaleDateString());
+// only return date when the gatedate() method call
+console.log(myDate.getDate());
+
