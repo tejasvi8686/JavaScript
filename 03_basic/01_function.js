9f6e0e5158b7c70d90c939ba0ab74b14f7fcb260
tejavi8686 <tejasviraj8686@gmail.com>
2024-07-31 23:13:22 +0530

    objects

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -27,7 +27,7 @@ console.log("Result:", result);
 // false converts true
 
 function loginUserMessage(usernames = "sam") {
-  if(!usernames) {
+  if (!usernames) {
     console.log("please enter a username");
     return;
   }
@@ -35,3 +35,40 @@ function loginUserMessage(usernames = "sam") {
 }
 
 console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num) {
+  return num;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000, 4000));
+
+
+// object
+const user = {
+  username: "tejasvi",
+  price: "19999",
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// both way to print the passing like objects in console like arguments
+// handleObject(user);
+handleObject({
+  username: "samhunt",
+  price: 200,
+});
+
+
+
+// array
+const myNewArray = [200, 400, 100, 600];
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 100]));
