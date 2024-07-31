05b6292273c47984dcddea451190cc8c4700fb32
tejavi8686 <tejasviraj8686@gmail.com>
2024-07-31 19:26:49 +0530

    object

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -29,34 +29,51 @@ const obj4 = { 5: "a", 6: "b" };
 // const obj3 = { obj1, obj2 };
 // const obj3 = Object.assign( {}, obj1, obj2, obj4)
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = { ...obj1, ...obj2 };
 
 console.log(obj3);
 
 const users = [
-    {
-        id:1,
-        email: "t@12gmail.com"
-    },
-    {
-        id:2,
-        email: "t@123gmail.com"
-    },
-    {
-        id:3,
-        email: "t@1234gmail.com"
-    },
-
-]
-
+  {
+    id: 1,
+    email: "t@12gmail.com",
+  },
+  {
+    id: 2,
+    email: "t@123gmail.com",
+  },
+  {
+    id: 3,
+    email: "t@1234gmail.com",
+  },
+];
 
 console.log(users[1].email);
-console.log(tinderUser); 
+console.log(tinderUser);
 
+// keys return only the keys.
 console.log(Object.keys(tinderUser));
+
+// values return only the values .
 console.log(Object.values(tinderUser));
+
+// entries retutrn both key and value .
 console.log(Object.entries(tinderUser));
 
-//hasOwnPropery retun the boolean values is it available or not 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//hasOwnPropery retun the boolean values is it available or not .
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+const course = {
+  coursesname: "js in hindi",
+  price: "999",
+  courseInstructor: "tejasviraj",
+};
+
+//console.courseInstructor
+
+const {courseInstructor: instructor} = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
+
 
