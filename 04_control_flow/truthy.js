5e2464cca321573972b2cf0351e2e55fae767f19
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-02 18:00:50 +0530

    truty values'

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,29 @@
+const userEmail = [];
+
+if (userEmail) {
+  console.log("Got user email");
+} else {
+  console.log("Don't have user email");
+}
+
+//false values
+// false , 0 , -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy value
+// "0" 'false', ' ', [] , {}, function () {}
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+const emptyObj = {};
+
+if (Object.keys(emptyObj).length === 0) {
+  console.log("Object is empty");
+}
+
+
+const a = 0;
+const b = ""
+
+console.log( a == b);
