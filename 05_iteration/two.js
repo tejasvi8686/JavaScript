f210e738d0227076479a7ba9d744a70f91ea6d68
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-03 09:29:01 +0530

    added dowhile loop and while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,27 @@
+// while loop
+
+let index = 0;
+
+while (index <= 10) {
+  console.log(`value of index ${index}`);
+  index = index + 2;
+}
+
+let myArray = ["flash", "batman", "superman"];
+
+let arr = 0;
+
+while (arr < myArray.length) {
+  console.log(`value is ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+
+// dowhile loop
+
+let score = 11;
+
+do {
+  console.log(`score is ${score}`);
+  score ++
+} while (score <= 10);
