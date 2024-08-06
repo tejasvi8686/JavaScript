091ecf4c4163ad6536d711a8a472d12e0e969c0c
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-06 09:55:41 +0530

    project_2 done

--- 07_projects/2-BMICalculator/script.js
+++ 07_projects/2-BMICalculator/script.js
@@ -1,49 +1,3 @@
-#projects related to DOM
-
-## project link
-
-[Click Here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution code
-
-#project 1
-
-``` Javascript
-
-const buttons = document.querySelectorAll(".button");
-const body = document.querySelector("body");
-
-buttons.forEach(function (button){
-    console.log(button);
-    button.addEventListener('click', function(e) {
-         console.log(e);
-         console.log(e.target);
-         if(e.target.id === 'grey') {
-            body.style.backgroundColor = e.target.id
-         }
-         if(e.target.id === 'white') {
-            body.style.backgroundColor = e.target.id
-         }
-         if(e.target.id === 'blue') {
-            body.style.backgroundColor = e.target.id
-         }
-         if(e.target.id === 'yellow') {
-            body.style.backgroundColor = e.target.id
-         }
-         
-         
-    })
-    
-})
-
-
-```
-
-#project 2
-
-```javascript
-
-
 const form = document.querySelector('#bmi-form');
 
 form.addEventListener('submit', function (e) {
@@ -75,5 +29,3 @@ form.addEventListener('submit', function (e) {
     bmiGuide.innerHTML = `<span>${guideMessage}</span>`;
   }
 });
-
-```
