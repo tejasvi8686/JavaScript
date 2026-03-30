8c08d1445a56d1ec4506f4f7f1b17ea48cc46eb4
tajsviappeneure <105453831+tejasvi8686@users.noreply.github.com>
2026-03-30 22:36:59 +0530

    practise normal queston

---
+++ Practise/1.js
@@ -0,0 +1,23 @@
+// question 1
+
+// const a = [1, 2];
+// const b = [3, 4];
+// const c = [...a, ...b, 5];
+// console.log(c);
+
+
+//question 2
+
+// const arr = [1, 2, 3, 4, 5];
+
+
+// const result = arr
+//    .filter(x => x % 2 === 0)
+//   .map(x => x * 3);
+
+//   console.log(result);
+
+  const a = 1 => 1 % 2 === 0
+
+  console.log("a", a);
+  
