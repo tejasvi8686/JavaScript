4e79451213fa108f6ef46ee0092c54879ef752ca
tajsviappeneure <105453831+tejasvi8686@users.noreply.github.com>
2026-03-30 22:37:14 +0530

    update

--- Practise/1.js
+++ Practise/1.js
@@ -1,23 +1,22 @@
 // question 1
 
-// const a = [1, 2];
-// const b = [3, 4];
-// const c = [...a, ...b, 5];
-// console.log(c);
+const a = [1, 2];
+const b = [3, 4];
+const c = [...a, ...b, 5];
+console.log(c);
 
 
 //question 2
 
-// const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5];
 
 
-// const result = arr
-//    .filter(x => x % 2 === 0)
-//   .map(x => x * 3);
+const result = arr
+   .filter(x => x % 2 === 0)
+  .map(x => x * 3);
 
-//   console.log(result);
+  console.log(result);
 
-  const a = 1 => 1 % 2 === 0
 
-  console.log("a", a);
+
   
